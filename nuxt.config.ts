696774df97c320a57ab18a9da291e63473619fd1
Chi Gang <loyalyonggang@gmail.com>
2025-12-24 16:22:37 +0800

    fix: enable SSR to match Netlify deployment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxtjs/ionic',
   ],
-  ssr: false,
+  ssr: true,
 
   components: [
     { path: '~/components', pathPrefix: false },
